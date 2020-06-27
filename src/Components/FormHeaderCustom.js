2af6c58177b79d932f1cee1ba46d8ce4958c14cb
anshusaurav <anshu.saurav@gmail.com>
2020-06-27 20:01:41 +0530

    Login Form 2/5

--- src/Components/FormHeaderCustom.js
+++ src/Components/FormHeaderCustom.js
@@ -1,7 +1,7 @@
 
 import styled from 'styled-components'
 
-const FormHeaderCustom = styled.button`
+const FormHeaderCustom = styled.h1`
   font-size: 3.5em;
   text-align: center;
   color: rgba(7, 8, 9, 0.85);
--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,50 +1,72 @@
 import React from 'react'
-import GlobalFonts from './../fonts/fonts';
-import  FormHeaderCustom from './FormHeaderCustom'
-import { Button, Form, Grid, Message, Segment } from 'semantic-ui-react'
-
-
+import GlobalFonts from './../fonts/fonts'
+import FormHeaderCustom from './FormHeaderCustom'
+import {
+  Button,
+  Form,
+  Grid,
+  Message,
+  Segment,
+  Divider,
+  Icon
+} from 'semantic-ui-react'
 
 class LoginForm extends React.Component {
-//   state = { name: '', email: '', submittedName: '', submittedEmail: '' }
+  //   state = { name: '', email: '', submittedName: '', submittedEmail: '' }
 
-//   handleChange = (e, { name, value }) => this.setState({ [name]: value })
+  //   handleChange = (e, { name, value }) => this.setState({ [name]: value })
 
-//   handleSubmit = () => {
-//     const { name, email } = this.state
+  //   handleSubmit = () => {
+  //     const { name, email } = this.state
 
-//     this.setState({ submittedName: name, submittedEmail: email })
-//   }
+  //     this.setState({ submittedName: name, submittedEmail: email })
+  //   }
 
   render () {
     return (
-        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-        <Grid.Column style={{ maxWidth: 450 }}>
-        <GlobalFonts/>
-          <FormHeaderCustom>
-            Instagram
-          </FormHeaderCustom>
-          <Form size='large'>
-            <Segment stacked>
-              <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-              <Form.Input
-                fluid
-                icon='lock'
-                iconPosition='left'
-                placeholder='Password'
-                type='password'
-              />
-    
-              <Button color='teal' fluid size='large'>
-                Login
-              </Button>
-            </Segment>
-          </Form>
-          <Message>
-            New to us? Sign Up
-          </Message>
-        </Grid.Column>
-      </Grid>
+      <div class='form-container'>
+        <Grid
+          textAlign='center'
+          style={{ height: '100vh' }}
+          verticalAlign='middle'
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <GlobalFonts />
+            <FormHeaderCustom>Instagram</FormHeaderCustom>
+            <Form size='large'>
+              <Segment>
+                <Form.Input
+                  fluid
+                  icon='user'
+                  iconPosition='left'
+                  placeholder='E-mail address'
+                />
+                <Form.Input
+                  fluid
+                  icon='lock'
+                  iconPosition='left'
+                  placeholder='Password'
+                  type='password'
+                />
+
+                <Button color='blue' fluid size='large'>
+                  Log in
+                </Button>
+              </Segment>
+            </Form>
+            <Divider horizontal>Or</Divider>
+            <div className='login-facebook-btn-div'>
+            <Button>
+              <Icon name='facebook square' color='blue'></Icon>Log in with
+              Facebook
+            </Button>
+            </div>
+            <Message>
+              Don't have an account? <Button>Sign Up</Button>
+            </Message>
+          </Grid.Column>
+        </Grid>
+      </div>
     )
   }
 }
