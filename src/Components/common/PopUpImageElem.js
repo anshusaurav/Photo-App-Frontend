8861fc5f6c671bdd71d9717dc0450283322df335
anshusaurav <anshu.saurav@gmail.com>
2020-07-01 18:21:27 +0530

    PopUp Basic UI done

--- src/Components/common/PopUpImageElem.js
+++ src/Components/common/PopUpImageElem.js
@@ -27,108 +27,134 @@ class PopUpImageElem extends React.Component {
     ]
     const img = this.props.img
     return (
-      <Card className='popup-image-card' style={{backgroundColor: 'transparent', minHeight: '100vh', borderRadius: '1px'}}>
-        <div className='pop-up-container' >
-        <div className='pop-up-grid'>
-          <Image className='popup-image-elem' src={img} wrapped ui={true} />
-          <div>
-            <Card.Content className='popup-image-author-content'>
-              <div className='popup-image-author-profile'>
-                <Image
-                  className='popup-image-author-image'
-                  src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
-                  size='mini'
-                  circular
-                />
-                <span className='popup-image-author-username'>anshusaurav</span>
+      <Card
+        className='popup-image-card'
+        style={{
+          backgroundColor: 'transparent',
+          minHeight: '100vh',
+          borderRadius: '1px'
+        }}
+      >
+        <div className='pop-up-container'>
+          <div className='pop-up-grid'>
+            <Image className='popup-image-elem' src={img} wrapped ui={true} />
+            <div className='pop-up-des-div'>
+              <Card.Content className='popup-image-author-content'>
+                <div className='popup-image-author-profile'>
+                  <Image
+                    className='popup-image-author-image'
+                    src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
+                    size='mini'
+                    circular
+                  />
+                  <span className='popup-image-author-username'>
+                    anshusaurav
+                  </span>
+                </div>
+                <span>
+                  {' '}
+                  <Icon name='ellipsis horizontal' />{' '}
+                </span>
+              </Card.Content>
+              <Card.Content>
+                
+                <Card.Description className='popup-common-des  popup-comment-elem-single'>
+                  <Image
+                    size='mini'
+                    className='popup-comment-comment-user-img'
+                    src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg'
+                  ></Image>
+                  <strong>anshusaurav </strong>
+                  Why everyone linking this pic..??
+                </Card.Description>
+                <Card.Description className='popup-common-des popup-comment-elem-single'>
+                  <Image
+                    size='mini'
+                    className='popup-comment-comment-user-img'
+                    src='https://react.semantic-ui.com/images/avatar/small/matthew.png'
+                  ></Image>
+                  <strong>Sunny </strong>
+                  Bela paisagem! !! Vamos segui? SDV!
+                </Card.Description>
+                <Card.Description className='popup-common-des popup-comment-elem-single '>
+                  <Image
+                    size='mini'
+                    className='popup-comment-comment-user-img'
+                    src='https://react.semantic-ui.com/images/avatar/small/lindsay.png'
+                  ></Image>
+                  <strong>Leone </strong>
+                  is a comedian living in Nashville.
+                </Card.Description>
+                <Card.Meta className='popup-common-des popup-more-comment-anchor'>
+                  View All 106 comments
+                </Card.Meta>
+                <Card.Description className='popup-image-inter-content'>
+                  <div className='popup-image-action-div'>
+                    <span>
+                      {' '}
+                      <Icon
+                        className='popup-action-elem'
+                        name='heart outline large'
+                      />{' '}
+                    </span>
+                    <span>
+                      {' '}
+                      <Icon
+                        className='popup-action-elem'
+                        name='comment outline large'
+                      />{' '}
+                    </span>
+                    <span>
+                      {' '}
+                      <Icon
+                        className='popup-action-elem'
+                        name='send outline large'
+                      />{' '}
+                    </span>
+                  </div>
+
+                  <div>
+                    <span>
+                      {' '}
+                      <Icon
+                        className='popup-action-elem'
+                        name='bookmark outline large'
+                      />{' '}
+                    </span>
+                  </div>
+                </Card.Description>
+                <Card.Description className='popup-common-des'>
+                  {' '}
+                  <p className='popup-image-like-count'>1,022 likes</p>
+                </Card.Description>
+                <Card.Description className='popup-image-elem-date popup-common-des'>
+                  2 days ago
+                </Card.Description>
+              </Card.Content>
+              <div className='popup-comment-form-outer-div'>
+                <Form className='popup-add-comment-form'>
+                  <TextArea
+                    rows={1}
+                    class='popup-add-comment-input-text'
+                    placeholder='Add a comment...'
+                    style={{
+                      border: 0,
+                      overflow: 'hidden',
+                      width: '100%',
+                      resize: 'none'
+                    }}
+                  />
+
+                  <Button
+                    class='popup-elem-add-comment-btn'
+                    // disabled={true}
+                    style={{ background: 'none', color: '#0095f6' }}
+                  >
+                    POST
+                  </Button>
+                </Form>
               </div>
-              <span>
-                {' '}
-                <Icon name='ellipsis horizontal' />{' '}
-              </span>
-            </Card.Content>
-            <Card.Content>
-              <Card.Description>
-                {' '}
-                <p className='popup-image-like-count'>1,022 likes</p>
-              </Card.Description>
-              <Card.Meta>View All 106 comments</Card.Meta>
-              <Card.Description>
-                <strong>anshusaurav </strong>
-                Why everyone linking this pic..??
-              </Card.Description>
-              <Card.Description>
-                <strong>Sunny </strong>
-                Bela paisagem! !! Vamos segui? SDV!
-              </Card.Description>
-              <Card.Description>
-                <strong>Leone </strong>
-                is a comedian living in Nashville.
-              </Card.Description>
-
-              <Card.Description className='popup-image-elem-date'>
-                2 days ago
-              </Card.Description>
-              <Card.Description className='popup-image-inter-content'>
-                <div className='popup-image-action-div'>
-                  <span>
-                    {' '}
-                    <Icon
-                      className='popup-action-elem'
-                      name='heart outline large'
-                    />{' '}
-                  </span>
-                  <span>
-                    {' '}
-                    <Icon
-                      className='popup-action-elem'
-                      name='comment outline large'
-                    />{' '}
-                  </span>
-                  <span>
-                    {' '}
-                    <Icon
-                      className='popup-action-elem'
-                      name='send outline large'
-                    />{' '}
-                  </span>
-                </div>
-
-                <div>
-                  <span>
-                    {' '}
-                    <Icon
-                      className='popup-action-elem'
-                      name='bookmark outline large'
-                    />{' '}
-                  </span>
-                </div>
-              </Card.Description>
-            </Card.Content>
-            <div className='popup-comment-form-outer-div'>
-              <Form className='popup-add-comment-form'>
-                <TextArea
-                  rows={1}
-                  class='popup-add-comment-input-text'
-                  placeholder='Add a comment...'
-                  style={{
-                    border: 0,
-                    overflow: 'hidden',
-                    width: '100%',
-                    resize: 'none'
-                  }}
-                />
-
-                <Button
-                  class='popup-elem-add-comment-btn'
-                  // disabled={true}
-                  style={{ background: 'none', color: '#0095f6' }}
-                >
-                  POST
-                </Button>
-              </Form>
             </div>
-          </div>
           </div>
         </div>
       </Card>
