ff0136ef5b2e49f0c78d51c73d0d51445810fc14
anshusaurav <anshu.saurav@gmail.com>
2020-07-02 16:23:27 +0530

    Minor Changes

--- src/Components/common/PopUpImageElem.js
+++ src/Components/common/PopUpImageElem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createRef } from 'react'
 import { Card, Icon, Image, Form, TextArea, Button } from 'semantic-ui-react'
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en'
@@ -10,12 +10,67 @@ class PopUpImageElem extends React.Component {
       body: '',
       comments: null,
       isUpdated: false,
-      img:null
+      img: null
     }
+    this.textAreaRef = createRef()
     this.changeHandler = this.changeHandler.bind(this)
     this.submitHandler = this.submitHandler.bind(this)
+    this.toggleLike = this.toggleLike.bind(this)
+    this.putFocusOnTextArea = this.putFocusOnTextArea.bind(this)
   }
 
+  putFocusOnTextArea (event) {
+    console.dir(this.textAreaRef.current)
+    this.textAreaRef.current.focus()
+  }
+  toggleLike (event) {
+    event.preventDefault()
+    this.submitLikeToggle()
+  }
+  async submitLikeToggle () {
+    const slug = this.props.img
+    const { jwttoken } = localStorage
+    const url = `http://localhost:4000/api/p/${slug}/favorite`
+    const isLiked = this.state.img.favorited
+    // console.log('SEEHERE ANSHU',this.state.img);
+    // console.log('SEEHERE ANSHU',isLiked);
+    try {
+      let response
+      if (isLiked) {
+        response = await fetch(url, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Token ${jwttoken}`
+          }
+        })
+      } else {
+        response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Token ${jwttoken}`
+          }
+        })
+      }
+      let data = await response.json()
+      console.log(data)
+      if (!data.errors) {
+        this.setState({ isUpdated: !this.state.isUpdated })
+      } else {
+        const errors = []
+        for (const [key, value] of Object.entries(data.errors)) {
+          errors.push(`${key} ${value}`)
+        }
+        this.setState({ errorMsgs: errors })
+      }
+    } catch (error) {
+      console.error('Error:', error)
+      const errors = []
+      errors.push(error.toString())
+      this.setState({ errorMsgs: errors })
+    }
+  }
   changeHandler (event, { name, value }) {
     if (event.target.name === 'body') {
       this.setState({ body: event.target.value }, function () {
@@ -32,7 +87,7 @@ class PopUpImageElem extends React.Component {
     this.submitComment()
   }
   async submitComment () {
-    const  slug  = this.props.img;
+    const slug = this.props.img
     const { body } = this.state
     const { jwttoken } = localStorage
     const comment = { comment: { body } }
@@ -78,7 +133,7 @@ class PopUpImageElem extends React.Component {
     return { result: false, data }
   }
   async saveComments () {
-    const  slug  = this.props.img;
+    const slug = this.props.img
     const { jwttoken } = localStorage
     const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'))
     // console.log(loggedInUser.username)
@@ -93,7 +148,6 @@ class PopUpImageElem extends React.Component {
         }
       })
       const data = await response.json()
-      // console.log('comments', data)
       if (!data.errors) {
         this.setState({ comments: data.comments }, () => {
           let comments = [...this.state.comments]
@@ -109,8 +163,8 @@ class PopUpImageElem extends React.Component {
       console.error('Error: ' + error)
     }
   }
-  async saveImage(){
-    const  slug  = this.props.img;
+  async saveImage () {
+    const slug = this.props.img
     const { jwttoken } = localStorage
     const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'))
     // console.log(loggedInUser.username)
@@ -125,28 +179,29 @@ class PopUpImageElem extends React.Component {
         }
       })
       const data = await response.json()
-      console.log('image', data);
+      console.log('image', data)
       if (!data.errors) {
-        this.setState({ img: data.imagepost });
+        this.setState({ img: data.imagepost })
       }
     } catch (error) {
       console.error('Error: ' + error)
     }
   }
+
   componentDidMount () {
-    this.saveImage();
+    this.saveImage()
     this.saveComments()
   }
   componentDidUpdate (prevProps, prevState) {
     if (this.state.isUpdated !== prevState.isUpdated) {
       this.saveComments()
-      this.saveImage();
+      this.saveImage()
     }
   }
-  timeAgo(date){
+  timeAgo (date) {
     TimeAgo.addLocale(en)
-    const timeAgo = new TimeAgo('en-US');
-    return timeAgo.format(date);
+    const timeAgo = new TimeAgo('en-US')
+    return timeAgo.format(date)
   }
   render () {
     // let {
@@ -173,148 +228,163 @@ class PopUpImageElem extends React.Component {
           position: 'relative'
         }}
       >
-
         <span class='close-popup-span'>X</span>
         <div className='pop-up-container'>
           <div className='pop-up-grid'>
-            {this.state.img?
-            <Image
-              className='popup-image-elem'
-              src={`http://localhost:4000/${this.state.img.filename}`}
-              // wrapped
-              ui={true}
-            />:
-            <p>Loading</p>
-            }
-            {this.state.img && this.state.comments?
-            <div className='pop-up-des-div'>
-              <Card.Content className='popup-image-author-content'>
-                <div className='popup-image-author-profile'>
-                  <Image
-                    className='popup-image-author-image'
-                    src={this.state.img.author.image}
-                    size='mini'
-                    circular
-                  />
-                  <span className='popup-image-author-username'>
-                    {this.state.img.author.username}
+            {this.state.img ? (
+              <Image
+                className='popup-image-elem'
+                src={`http://localhost:4000/${this.state.img.filename}`}
+                ui={true}
+              />
+            ) : (
+              <p>Loading</p>
+            )}
+            {this.state.img && this.state.comments ? (
+              <div className='pop-up-des-div'>
+                <Card.Content className='popup-image-author-content'>
+                  <div className='popup-image-author-profile'>
+                    <Image
+                      className='popup-image-author-image'
+                      src={this.state.img.author.image}
+                      size='mini'
+                      circular
+                    />
+                    <span className='popup-image-author-username'>
+                      {this.state.img.author.username}
+                    </span>
+                  </div>
+                  <span>
+                    {' '}
+                    <Icon name='ellipsis horizontal' />{' '}
                   </span>
-                </div>
-                <span>
-                  {' '}
-                  <Icon name='ellipsis horizontal' />{' '}
-                </span>
-              </Card.Content>
-              <Card.Content>
-                <Card.Description className='popup-common-des  popup-comment-elem-single'>
-                  <Image
-                    size='mini'
-                    className='popup-comment-comment-user-img'
-                    src={this.state.img.author.image}
-                  ></Image>
-                  <strong>{this.state.img.author.username} </strong>
-                  {this.state.img.description}
-                </Card.Description>
-                {comments &&
-                  comments.map(comment => {
-                    return (
-                      <Card.Description className='popup-common-des popup-comment-elem-single'>
-                        <Image
-                          size='mini'
-                          className='popup-comment-comment-user-img'
-                          src={comment.author.image}
-                        ></Image>
-                        <strong>{comment.author.username}</strong>
-                        {comment.body}
-                      </Card.Description>
-                    )
-                  })}
+                </Card.Content>
+                <Card.Content>
+                  <Card.Description className='popup-common-des  popup-comment-elem-single'>
+                    <Image
+                      size='mini'
+                      className='popup-comment-comment-user-img'
+                      src={this.state.img.author.image}
+                    ></Image>
+                    <strong>{this.state.img.author.username} </strong>
+                    {this.state.img.description}
+                  </Card.Description>
+                  {comments &&
+                    comments.map(comment => {
+                      return (
+                        <Card.Description className='popup-common-des popup-comment-elem-single'>
+                          <Image
+                            size='mini'
+                            className='popup-comment-comment-user-img'
+                            src={comment.author.image}
+                          ></Image>
+                          <strong>{comment.author.username}</strong>
+                          {comment.body}
+                        </Card.Description>
+                      )
+                    })}
 
-                <Card.Meta className='popup-common-des popup-more-comment-anchor'>
-                  {this.state.img.commentsCount !== 0
-                    ? `View All ${this.state.img.commentsCount} comments`
-                    : `Be the first one to respond`}
-                </Card.Meta>
-                <Card.Description className='popup-image-inter-content'>
-                  <div className='popup-image-action-div'>
-                    <span>
-                      {' '}
-                      <Icon
-                        className='popup-action-elem'
-                        name='heart outline large'
-                      />{' '}
-                    </span>
-                    <span>
-                      {' '}
-                      <Icon
-                        className='popup-action-elem'
-                        name='comment outline large'
-                      />{' '}
-                    </span>
-                    <span>
-                      {' '}
-                      <Icon
-                        className='popup-action-elem'
-                        name='send outline large'
-                      />{' '}
-                    </span>
-                  </div>
+                  <Card.Meta className='popup-common-des popup-more-comment-anchor'>
+                    {this.state.img.commentsCount !== 0
+                      ? `View All ${this.state.img.commentsCount} ${
+                          this.state.img.commentsCount !== 1
+                            ? 'comments'
+                            : 'comment'
+                        }`
+                      : `Be the first one to respond`}
+                  </Card.Meta>
+                  <Card.Description className='popup-image-inter-content'>
+                    <div className='popup-image-action-div'>
+                      <span>
+                        {' '}
+                        <Icon
+                          className='popup-action-elem'
+                          name={
+                            this.state.img.favorited
+                              ? 'heart large red'
+                              : 'heart outline large'
+                          }
+                          onClick={this.toggleLike}
+                        />{' '}
+                      </span>
+                      <span>
+                        {' '}
+                        <Icon
+                          className='popup-action-elem'
+                          name='comment outline large'
+                          onClick={this.putFocusOnTextArea}
+                        />{' '}
+                      </span>
+                      <span>
+                        {' '}
+                        <Icon
+                          className='popup-action-elem'
+                          name='send outline large'
+                        />{' '}
+                      </span>
+                    </div>
 
-                  <div>
-                    <span>
-                      {' '}
-                      <Icon
-                        className='popup-action-elem'
-                        name='bookmark outline large'
-                      />{' '}
-                    </span>
-                  </div>
-                </Card.Description>
-                <Card.Description className='popup-common-des'>
-                  {' '}
-                  <p className='popup-image-like-count'>
-                    {this.state.img.favoritesCount !== 0
-                      ? `${this.state.img.favoritesCount} likes`
-                      : `Be the first one to like`}
-                  </p>
-                </Card.Description>
-                <Card.Description className='popup-image-elem-date popup-common-des'>
-                {this.timeAgo(new Date(this.state.img.createdAt))}
-                </Card.Description>
-              </Card.Content>
-              <div className='popup-comment-form-outer-div'>
-                <Form
-                  className='popup-add-comment-form'
-                  onSubmit={this.submitHandler}
-                >
-                  <TextArea
-                    rows={1}
-                    class='popup-add-comment-input-text'
-                    placeholder='Add a comment...'
-                    name='body'
-                    onChange={this.changeHandler}
-                    value={body}
-                    style={{
-                      border: 0,
-                      overflow: 'hidden',
-                      width: '100%',
-                      resize: 'none'
-                    }}
-                  ></TextArea>
-
-                  <Button
-                    class='popup-elem-add-comment-btn'
-                    disabled={!isSubmitable}
-                    style={{ background: 'none', color: '#0095f6' }}
-                    onClick={this.submitHandler}
+                    <div>
+                      <span>
+                        {' '}
+                        <Icon
+                          className='popup-action-elem'
+                          name='bookmark outline large'
+                        />{' '}
+                      </span>
+                    </div>
+                  </Card.Description>
+                  <Card.Description className='popup-common-des'>
+                    {' '}
+                    <p className='popup-image-like-count'>
+                      {this.state.img.favoritesCount !== 0
+                        ? `${this.state.img.favoritesCount} ${
+                          this.state.img.favoritesCount !== 1
+                            ? 'likes'
+                            : 'like'
+                        }`
+                        : `Be the first one to like`}
+                    </p>
+                  </Card.Description>
+                  <Card.Description className='popup-image-elem-date popup-common-des'>
+                    {this.timeAgo(new Date(this.state.img.createdAt))}
+                  </Card.Description>
+                </Card.Content>
+                <div className='popup-comment-form-outer-div'>
+                  <Form
+                    className='popup-add-comment-form'
+                    onSubmit={this.submitHandler}
                   >
-                    POST
-                  </Button>
-                </Form>
+                    <TextArea
+                      rows={1}
+                      class='popup-add-comment-input-text'
+                      placeholder='Add a comment...'
+                      name='body'
+                      onChange={this.changeHandler}
+                      value={body}
+                      style={{
+                        border: 0,
+                        overflow: 'hidden',
+                        width: '100%',
+                        resize: 'none'
+                      }}
+                      ref={this.textAreaRef}
+                    ></TextArea>
+
+                    <Button
+                      class='popup-elem-add-comment-btn'
+                      disabled={!isSubmitable}
+                      style={{ background: 'none', color: '#0095f6' }}
+                      onClick={this.submitHandler}
+                    >
+                      POST
+                    </Button>
+                  </Form>
+                </div>
               </div>
-            </div>
-            :<p>Loading</p>
-            }
+            ) : (
+              <p>Loading</p>
+            )}
           </div>
         </div>
       </Card>
