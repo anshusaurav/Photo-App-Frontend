e1b7d688cdb6bfcb45cfd3f73f0be0ccc18c4e00
anshusaurav <anshu.saurav@gmail.com>
2020-07-03 13:48:35 +0530

    Homepage feed images

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -4,44 +4,35 @@ import HeaderNav from './common/HeaderNav'
 import { Button } from 'semantic-ui-react'
 import FeedImageElem from './common/FeedImageElem'
 class HomePage extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { imagepostList: null }
+  }
+  async savePosts () {
+    const { jwttoken } = localStorage
+    const url = `http://localhost:4000/api/p/feed`
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/JSON',
+          Authorization: `Token ${jwttoken}`
+        }
+      })
+      const data = await response.json();
+      console.log(data);
+      if (!data.errors) {
+        this.setState({ imagepostList: data.imageposts })
+      }
+    } catch (error) {
+      console.error('Error: ' + error)
+    }
+  }
+  componentDidMount () {
+    this.savePosts()
+  }
+
   render () {
-    var arr = [
-      'https://images.pexels.com/photos/3088778/pexels-photo-3088778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/1628233/pexels-photo-1628233.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/1051075/pexels-photo-1051075.jpeg?cs=srgb&dl=person-pointing-at-black-and-gray-film-camera-near-macbook-1051075.jpg&fm=jpg',
-      'https://images.pexels.com/photos/62689/pexels-photo-62689.jpeg?cs=srgb&dl=camera-iphone-macbook-pro-office-62689.jpg&fm=jpg',
-      'https://images.pexels.com/photos/2130134/pexels-photo-2130134.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      'https://images.pexels.com/photos/917499/pexels-photo-917499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      'https://images.pexels.com/photos/917499/pexels-photo-917499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      'https://images.pexels.com/photos/1117862/pexels-photo-1117862.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      'https://images.pexels.com/photos/372882/pexels-photo-372882.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      'https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      'https://images.pexels.com/photos/247599/pexels-photo-247599.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      'https://images.pexels.com/photos/1262302/pexels-photo-1262302.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/1295572/pexels-photo-1295572.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      'https://images.pexels.com/photos/3184431/pexels-photo-3184431.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/1485548/pexels-photo-1485548.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/267507/pexels-photo-267507.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      'https://images.pexels.com/photos/4629633/pexels-photo-4629633.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      'https://images.pexels.com/photos/4681113/pexels-photo-4681113.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/4681074/pexels-photo-4681074.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      'https://images.pexels.com/photos/4614167/pexels-photo-4614167.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      'https://images.pexels.com/photos/917499/pexels-photo-917499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      'https://images.pexels.com/photos/4614516/pexels-photo-4614516.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/4593876/pexels-photo-4593876.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/4623636/pexels-photo-4623636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/4710891/pexels-photo-4710891.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/4644397/pexels-photo-4644397.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/732423/pexels-photo-732423.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/1329308/pexels-photo-1329308.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/352096/pexels-photo-352096.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/3616232/pexels-photo-3616232.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/3358967/pexels-photo-3358967.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/3617467/pexels-photo-3617467.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/3636074/pexels-photo-3636074.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/3369526/pexels-photo-3369526.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      'https://images.pexels.com/photos/3369526/pexels-photo-3369526.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-    ]
     var suggestions = [
       {
         image: 'https://react.semantic-ui.com/images/avatar/small/veronika.jpg',
@@ -64,37 +55,22 @@ class HomePage extends React.Component {
         name: 'rachael'
       }
     ]
-    const {toggleLoggedIn} = this.props;
+    const { toggleLoggedIn } = this.props;
+    const {imagepostList} = this.state;
     return (
-      
       <div className='full-container'>
-        <HeaderNav toggleLoggedIn={toggleLoggedIn}/>
+        <HeaderNav toggleLoggedIn={toggleLoggedIn} />
         <div>
           <div className='container'>
             <div className='home-page-div'>
               <div className='feed-images-div'>
-                {arr.map(img => {
-                  return (
-                   <FeedImageElem img={img}/>
-                      
-                  )
+                {imagepostList && imagepostList.map(img => {
+                  return <FeedImageElem img={img.slug} />
                 })}
               </div>
               <div className='feed-suggestions-div'>
                 <div className='feed-suggestion-inner-div'>
                   <div className='feed-profile-div'>
-                    {/* <Card>
-                      <Card.Content>
-                        <Image
-                          className='feed-profile-picture-img'
-                          floated='right'
-                          size='mini'
-                          src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
-                        />
-                        <Card.Header>anshusaurav</Card.Header>
-                        <Card.Meta>Anshu Saurabh</Card.Meta>
-                      </Card.Content>
-                    </Card> */}
                     <div className='feed-profile-inner-div'></div>
                   </div>
                   <div className='feed-sugg-user-div'>
--- src/Components/common/FeedImageElem.js
+++ src/Components/common/FeedImageElem.js
@@ -1,105 +1,425 @@
-import React from 'react'
-import { Card, Icon, Image, Form, TextArea, Button } from 'semantic-ui-react'
-
+import React, { createRef } from 'react'
+import {
+  Card,
+  Icon,
+  Image,
+  Form,
+  TextArea,
+  Button,
+  Transition
+} from 'semantic-ui-react'
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
 class FeedImageElem extends React.Component {
-  
+  constructor (props) {
+    super(props)
+    this.state = {
+      isSubmitable: '',
+      body: '',
+      comments: null,
+      isUpdated: false,
+      img: null,
+      animation: 'tada',
+      duration: 500,
+      visible: true
+    }
+    this.textAreaRef = createRef()
+    this.changeHandler = this.changeHandler.bind(this)
+    this.submitHandler = this.submitHandler.bind(this)
+    this.toggleLike = this.toggleLike.bind(this)
+    this.putFocusOnTextArea = this.putFocusOnTextArea.bind(this)
+    this.toggleFollow = this.toggleFollow.bind(this)
+  }
+  toggleFollow (event) {
+    event.preventDefault()
+    this.submitFollowToggle()
+  }
+  async submitFollowToggle () {
+    const slug = this.state.img.author.username
+    const { jwttoken } = localStorage
+    const url = `http://localhost:4000/api/profiles/${slug}/follow`
+    const isFollowed = this.state.img.author.following
+    try {
+      let response
+      if (isFollowed) {
+        response = await fetch(url, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Token ${jwttoken}`
+          }
+        })
+      } else {
+        response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Token ${jwttoken}`
+          }
+        })
+      }
+      let data = await response.json()
+      console.log(data)
+      if (!data.errors) {
+        this.setState({ isUpdated: !this.state.isUpdated })
+        this.toggleVisibility()
+      } else {
+        const errors = []
+        for (const [key, value] of Object.entries(data.errors)) {
+          errors.push(`${key} ${value}`)
+        }
+        this.setState({ errorMsgs: errors })
+      }
+    } catch (error) {
+      console.error('Error:', error)
+      const errors = []
+      errors.push(error.toString())
+      this.setState({ errorMsgs: errors })
+    }
+  }
+  toggleVisibility = () =>
+    this.setState(prevState => ({ visible: !prevState.visible }))
+
+  putFocusOnTextArea (event) {
+    console.dir(this.textAreaRef.current)
+    this.textAreaRef.current.focus()
+  }
+  toggleLike (event) {
+    event.preventDefault()
+    this.submitLikeToggle()
+  }
+  async submitLikeToggle () {
+    const slug = this.props.img
+    const { jwttoken } = localStorage
+    const url = `http://localhost:4000/api/p/${slug}/favorite`
+    const isLiked = this.state.img.favorited
+    try {
+      let response
+      if (isLiked) {
+        response = await fetch(url, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Token ${jwttoken}`
+          }
+        })
+      } else {
+        response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Token ${jwttoken}`
+          }
+        })
+      }
+      let data = await response.json()
+      // console.log(data)
+      if (!data.errors) {
+        this.setState({ isUpdated: !this.state.isUpdated })
+        this.toggleVisibility()
+      } else {
+        const errors = []
+        for (const [key, value] of Object.entries(data.errors)) {
+          errors.push(`${key} ${value}`)
+        }
+        this.setState({ errorMsgs: errors })
+      }
+    } catch (error) {
+      console.error('Error:', error)
+      const errors = []
+      errors.push(error.toString())
+      this.setState({ errorMsgs: errors })
+    }
+  }
+  changeHandler (event, { name, value }) {
+    if (event.target.name === 'body') {
+      this.setState({ body: event.target.value }, function () {
+        if (this.checkValidComment().result) {
+          this.setState({ isSubmitable: true })
+        } else {
+          this.setState({ isSubmitable: false })
+        }
+      })
+    }
+  }
+  submitHandler (event) {
+    event.preventDefault()
+    this.submitComment()
+  }
+  async submitComment () {
+    const slug = this.props.img
+    const { body } = this.state
+    const { jwttoken } = localStorage
+    const comment = { comment: { body } }
+    const url = `http://localhost:4000/api/p/${slug}/comments`
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Token ${jwttoken}`
+        },
+        body: JSON.stringify(comment)
+      })
+      let data = await response.json()
+      if (!data.errors) {
+        this.setState({ body: '' })
+        this.setState({ isUpdated: !this.state.isUpdated })
+      } else {
+        const errors = []
+        for (const [key, value] of Object.entries(data.errors)) {
+          errors.push(`${key} ${value}`)
+        }
+        this.setState({ errorMsgs: errors })
+      }
+    } catch (error) {
+      console.error('Error:', error)
+      const errors = []
+      errors.push(error.toString())
+      this.setState({ errorMsgs: errors })
+    }
+  }
+  checkValidComment () {
+    const { body } = this.state
+    let res = true,
+      data = []
+    if (body.trim().length === 0) {
+      res = false
+      data.push('email')
+    }
+    if (res) return { result: true, data }
+
+    return { result: false, data }
+  }
+  async saveComments () {
+    const slug = this.props.img
+    const { jwttoken } = localStorage
+    const url = `http://localhost:4000/api/p/${slug}/comments`
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/JSON',
+          Authorization: `Token ${jwttoken}`
+        }
+      })
+      const data = await response.json()
+      if (!data.errors) {
+        this.setState({ comments: data.comments }, () => {
+          let comments = [...this.state.comments]
+          comments.forEach(comment => {
+            TimeAgo.addLocale(en)
+            const timeAgo = new TimeAgo('en-US')
+            comment.createdAt = timeAgo.format(new Date(comment.createdAt))
+          })
+          this.setState({ comments })
+        })
+      }
+    } catch (error) {
+      console.error('Error: ' + error)
+    }
+  }
+  async saveImage () {
+    const slug = this.props.img
+    const { jwttoken } = localStorage
+    const url = `http://localhost:4000/api/p/${slug}`
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/JSON',
+          Authorization: `Token ${jwttoken}`
+        }
+      })
+      const data = await response.json()
+      console.log('image', data)
+      if (!data.errors) {
+        this.setState({ img: data.imagepost })
+      }
+    } catch (error) {
+      console.error('Error: ' + error)
+    }
+  }
+
+  componentDidMount () {
+    this.saveImage()
+    this.saveComments()
+  }
+  componentDidUpdate (prevProps, prevState) {
+    if (this.state.isUpdated !== prevState.isUpdated) {
+      this.saveComments()
+      this.saveImage()
+    }
+  }
+  timeAgo (date) {
+    TimeAgo.addLocale(en)
+    const timeAgo = new TimeAgo('en-US')
+    return timeAgo.format(date)
+  }
   render () {
-      const img = this.props.img;
+    const {
+      isSubmitable,
+      body,
+      comments,
+      animation,
+      duration,
+      visible
+    } = this.state
+    const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'))
     return (
       <Card className='feed-image-card'>
-        <Card.Content className='feed-image-author-content'>
-          <div className='feed-image-author-profile'>
-            <Image
-              className='feed-image-author-image'
-              src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
-              size='mini'
-              circular
-            />
-            <span className='feed-image-author-username'>anshusaurav</span>
-          </div>
-          <span>
-            {' '}
-            <Icon name='ellipsis horizontal' />{' '}
-          </span>
-        </Card.Content>
-        <Image className='feed-image-elem' src={img} wrapped ui={true} />
-        <Card.Content>
-          <Card.Description className='feed-image-inter-content'>
-            <div className='feed-image-action-div'>
-              <span>
-                {' '}
-                <Icon className='action-elem' name='heart outline large' />{' '}
-              </span>
-              <span>
-                {' '}
-                <Icon
-                  className='action-elem'
-                  name='comment outline large'
-                />{' '}
-              </span>
-              <span>
-                {' '}
-                <Icon className='action-elem' name='send outline large' />{' '}
+        {this.state.img ? (
+          <Card.Content className='feed-image-author-content'>
+            <div className='feed-image-author-profile'>
+              <Image
+                className='feed-image-author-image'
+                src={this.state.img.author.image}
+                size='mini'
+                circular
+              />
+              <span className='feed-image-author-username'>
+                {this.state.img.author.username}
               </span>
             </div>
+            <span>
+              {' '}
+              <Icon name='ellipsis horizontal' />{' '}
+            </span>
+          </Card.Content>
+        ) : (
+          <p>Loading...</p>
+        )}
+        {this.state.img ? (
+          <Image
+            className='feed-image-elem'
+            src={`http://localhost:4000/${this.state.img.filename}`}
+            wrapped
+            ui={true}
+          />
+        ) : (
+          <p>Loading...</p>
+        )}
+        {this.state.img && this.state.comments ? (
+          <>
+            <Card.Content>
+              <Card.Description className='feed-image-inter-content'>
+                <div className='feed-image-action-div'>
+                  <span>
+                    <Transition
+                      animation={animation}
+                      duration={duration}
+                      visible={visible}
+                    >
+                      <Icon
+                        className='action-elem'
+                        name={
+                          this.state.img.favorited
+                            ? 'heart large red'
+                            : 'heart outline large'
+                        }
+                        onClick={this.toggleLike}
+                      />
+                    </Transition>
+                  </span>
+                  <span>
+                    {' '}
+                    <Icon
+                      className='action-elem'
+                      name='comment outline large'
+                      onClick={this.putFocusOnTextArea}
+                    />
+                  </span>
+                  <span>
+                    {' '}
+                    <Icon
+                      className='action-elem'
+                      name='send outline large'
+                    />{' '}
+                  </span>
+                </div>
 
-            <div>
-              <span>
+                <div>
+                  <span>
+                    {' '}
+                    <Icon
+                      className='action-elem'
+                      name='bookmark outline large'
+                    />{' '}
+                  </span>
+                </div>
+              </Card.Description>
+              <Card.Description>
                 {' '}
-                <Icon
-                  className='action-elem'
-                  name='bookmark outline large'
-                />{' '}
-              </span>
+                <p className='feed-image-like-count'>
+                  {this.state.img.favoritesCount !== 0
+                    ? `${this.state.img.favoritesCount} ${
+                        this.state.img.favoritesCount !== 1 ? 'likes' : 'like'
+                      }`
+                    : `Be the first one to like`}
+                </p>
+              </Card.Description>
+              <Card.Meta>
+                {this.state.img.commentsCount !== 0
+                  ? `View All ${this.state.img.commentsCount} ${
+                      this.state.img.commentsCount !== 1
+                        ? 'comments'
+                        : 'comment'
+                    }`
+                  : `Be the first one to respond`}
+              </Card.Meta>
+              <Card.Description>
+                <strong>{this.state.img.author.username} </strong>
+                {this.state.img.description}
+              </Card.Description>
+              {comments &&
+                comments.map(comment => {
+                  return (
+                    <Card.Description>
+                      <strong>{comment.author.username} </strong>
+                      {comment.body}
+                    </Card.Description>
+                  )
+                })}
+
+              <Card.Description className='feed-image-elem-date'>
+                2 days ago
+              </Card.Description>
+            </Card.Content>
+            <div className='comment-form-outer-div'>
+              <Form className='add-comment-form' onSubmit = {this.submitHandler}>
+                <TextArea
+                  rows={1}
+                  name='body'
+                  onChange={this.changeHandler}
+                  value={this.state.body}
+                  class='add-comment-input-text'
+                  placeholder='Add a comment...'
+                  style={{
+                    border: 0,
+                    overflow: 'hidden',
+                    width: '100%',
+                    resize: 'none'
+                  }}
+                  ref={this.textAreaRef}
+                />
+
+                <Button
+                  class='feed-elem-add-comment-btn'
+                  disabled={!isSubmitable}
+                  style={{ background: 'none', color: '#0095f6' }}
+                  onClick={this.submitHandler}
+                >
+                  POST
+                </Button>
+              </Form>
             </div>
-          </Card.Description>
-          <Card.Description>
-            {' '}
-            <p className='feed-image-like-count'>1,022 likes</p>
-          </Card.Description>
-          <Card.Meta>View All 106 comments</Card.Meta>
-          <Card.Description>
-            <strong>anshusaurav </strong>
-            Why everyone linking this pic..??
-          </Card.Description>
-          <Card.Description>
-            <strong>Sunny </strong>
-            Bela paisagem! !! Vamos segui? SDV!
-          </Card.Description>
-          <Card.Description>
-            <strong>Leone </strong>
-            is a comedian living in Nashville.
-          </Card.Description>
-
-          <Card.Description className='feed-image-elem-date'>
-            2 days ago
-          </Card.Description>
-        </Card.Content>
-        <div className='comment-form-outer-div'>
-          <Form className='add-comment-form'>
-            <TextArea
-              rows={1}
-              class='add-comment-input-text'
-              placeholder='Add a comment...'
-              style={{
-                border: 0,
-                overflow: 'hidden',
-                width: '100%',
-                resize: 'none'
-              }}
-            />
-
-            <Button
-              class='feed-elem-add-comment-btn'
-              // disabled={true}
-              style={{ background: 'none', color: '#0095f6' }}
-            >
-              POST
-            </Button>
-          </Form>
-        </div>
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
       </Card>
     )
   }
 }
-export default FeedImageElem;
+export default FeedImageElem
