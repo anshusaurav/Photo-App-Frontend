186ced7064c71805f4f44a4269947e8020de498d
anshusaurav <anshu.saurav@gmail.com>
2020-07-19 19:37:33 +0530

    Profile Settings Form

--- src/Components/profile/EditProfile.js
+++ src/Components/profile/EditProfile.js
@@ -1,29 +1,51 @@
 import React from 'react'
-import {Button, TextArea} from 'semantic-ui-react';
-class EditProfile extends React.Component{
-
-    render(){
-        return (
-            <form>
-            <div className='edit-profile-section'>
-              <label>Name</label>
-              <input placeholder='First Name' />
-            </div>
-            <div className='edit-profile-section'>
-              <label>Username</label>
-              <input placeholder='anshusaurav' />
-            </div>
-            <div className='edit-profile-section'>
-              <label inline>Bio</label>
-              <TextArea placeholder='Last Name' inline/>
-            </div>
-            <div className='edit-profile-section'>
-              <label>Email</label>
-              <input placeholder='anshu.saurav@gmail.com' />
-            </div>
-            <Button type='submit' primary>Submit</Button>
-          </form>
-        );
-    }
+import { Button, TextArea, Input, Form} from 'semantic-ui-react'
+class EditProfile extends React.Component {
+  render () {
+    return (
+      <Form className='edit-profile-form'>
+        <div className='edit-profile-section'>
+          <div className='edit-profile-section-left'>
+            <label>Name</label>
+          </div>
+          <div className='edit-profile-section-right'>
+            <Input placeholder='First Name' />
+          </div>
+        </div>
+        <div className='edit-profile-section'>
+          <div className='edit-profile-section-left'>
+            <label>Username</label>
+          </div>
+          <div className='edit-profile-section-right'>
+            <Input placeholder='anshusaurav' />
+          </div>
+        </div>
+        <div className='edit-profile-section'>
+          <div className='edit-profile-section-left'>
+            <label inline>Bio</label>
+          </div>
+          <div className='edit-profile-section-right'>
+            <TextArea placeholder='Last Name' style={{ minHeight: 100 }}/>
+          </div>
+        </div>
+        <div className='edit-profile-section'>
+          <div className='edit-profile-section-left'>
+            <label>Email</label>
+          </div>
+          <div className='edit-profile-section-right'>
+            <Input placeholder='anshu.saurav@gmail.com' />
+          </div>
+        </div>
+        <div className='edit-profile-section'>
+          <div className='edit-profile-section-left'></div>
+          <div className='edit-profile-section-right'>
+            <Button type='submit' primary>
+              Submit
+            </Button>
+          </div>
+        </div>
+      </Form>
+    )
+  }
 }
-export default EditProfile;
+export default EditProfile
