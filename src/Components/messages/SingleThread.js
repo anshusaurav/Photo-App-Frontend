4861b19874180c6733cda4fb429f34170ef821bb
anshusaurav <anshu.saurav@gmail.com>
2020-08-04 22:08:01 +0530

    MessageThread

--- src/Components/messages/SingleThread.js
+++ src/Components/messages/SingleThread.js
@@ -2,13 +2,24 @@ import React from 'react'
 class SingleThread extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            msg: [],
+            numMessage: 0,
+            userProfile: null
+        }
     }
     componentDidMount() {
 
     }
+    componentDidUpdate(prevProps, prevstate) {
+        if (prevProps.user !== this.props.user) {
+        }
+    }
+    compoen
     render() {
         const { user } = this.props;
-        return <><p className='inbox-usercard-name'>{user.username}</p>
+        return <>
+            <p className='inbox-usercard-name'>{user}</p>
             <p className='inbox-usercard-time'>6 Hours Ago</p>
         </>
     }
